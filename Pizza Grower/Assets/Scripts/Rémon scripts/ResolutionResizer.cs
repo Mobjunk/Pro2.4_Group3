fcc91cd777acab23b8169c5ed1646cb0d7ea410b
Remon <remon@mail.com>
2020-04-09 11:07:17 +0200

    resolution settings and some project settings for android

---
+++ Pizza Grower/Assets/Scripts/Rémon scripts/ResolutionResizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionResizer : MonoBehaviour
+{
+  
+    public void Start()
+    {
+        float baseAspect = 1280.0f / 800.0f;
+
+        float currAspect  = 1.0f * Screen.width / Screen.height;
+        Camera.main.projectionMatrix = Matrix4x4.Scale(new Vector3(currAspect / baseAspect, 1.0f, 1.0f)) * Camera.main.projectionMatrix;
+    }
+}
