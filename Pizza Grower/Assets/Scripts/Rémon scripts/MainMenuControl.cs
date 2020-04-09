7661bb61db7f57d6ba0c3aefe6163ab69e50fe60
Remon <remon@mail.com>
2020-04-09 12:33:20 +0200

    trash main menu

---
+++ Pizza Grower/Assets/Scripts/Rémon scripts/MainMenuControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public GameObject optionsCanvas;
+    public GameObject highscoreCanvas;
+    public GameObject mainMenuCanas;
+    public GameObject custimizationCanas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenuCanas.SetActive(true);
+        optionsCanvas.SetActive(false);
+        highscoreCanvas.SetActive(false);
+        custimizationCanas.SetActive(false);
+    }
+
+    public void Play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        mainMenuCanas.SetActive(false);
+        optionsCanvas.SetActive(true);
+        highscoreCanvas.SetActive(false);
+        custimizationCanas.SetActive(false);
+    }
+
+    public void Highscore()
+    {
+        mainMenuCanas.SetActive(false);
+        optionsCanvas.SetActive(false);
+        highscoreCanvas.SetActive(true);
+        custimizationCanas.SetActive(false);
+    }
+
+    public void Custimization()
+    {
+        mainMenuCanas.SetActive(false);
+        optionsCanvas.SetActive(false);
+        highscoreCanvas.SetActive(false);
+        custimizationCanas.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenuCanas.SetActive(true);
+        optionsCanvas.SetActive(false);
+        highscoreCanvas.SetActive(false);
+        custimizationCanas.SetActive(false);
+    }
+
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+
+}
+
--- Pizza Grower/Assets/Scripts/Rémon scripts/ResolutionResizer.cs
+++ Pizza Grower/Assets/Scripts/Rémon scripts/ResolutionResizer.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class ResolutionResizer : MonoBehaviour
 {
-  
+    float baseAspect = 1280.0f / 800.0f;
+
+    float currAspect = 1.0f * Screen.width / Screen.height;
+
     public void Start()
     {
-        float baseAspect = 1280.0f / 800.0f;
-
-        float currAspect  = 1.0f * Screen.width / Screen.height;
         Camera.main.projectionMatrix = Matrix4x4.Scale(new Vector3(currAspect / baseAspect, 1.0f, 1.0f)) * Camera.main.projectionMatrix;
     }
 }
