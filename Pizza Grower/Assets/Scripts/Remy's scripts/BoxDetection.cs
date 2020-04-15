ab45492c9b69535be484a056d9e1aea440e63b6a
MarioPizzabakker <47322833+MarioPizzabakker@users.noreply.github.com>
2020-04-15 09:37:15 +0200

    PizzaBox Size checker
    de eerste stap voor het checken of die de box aanraakt

---
+++ Pizza Grower/Assets/Scripts/Remy's scripts/BoxDetection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDetection : MonoBehaviour
+{
+    //public BoxCollider2D pizzaBox;
+    public BoxCollider2D[] pizzaBox;
+    public CircleCollider2D pizza;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // private void OnCollisionEnter2D(Collision2D collision )
+    //  {
+    //if (collision.gameObject.name == "Pizza")
+    //{
+    //     Debug.Log("touched the box");
+    // }
+    //}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (other.gameObject.name == "Pizza")
+        //{
+            Debug.Log("touched the box");
+        //}
+    }
+
+}
---
+++ Pizza Grower/Assets/Scripts/Remy's scripts/PizzaGrower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PizzaGrower : MonoBehaviour
+{
+    Vector3 size;
+    Vector3 rotation = new Vector3(0f, 0f, 100f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = this.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("it works");
+            size.y += 0.05f;
+            size.x += 0.05f;
+            this.transform.Rotate(rotation * Time.deltaTime);
+            this.transform.localScale = size;
+        }
+    }
+}
