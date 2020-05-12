ac5fbe2721f027d6cf70862401a71fdfe1da4d4b
SmallEnderman <likeaendermanplaysminecraft@gmail.com>
2020-05-12 13:10:39 +0200

    FINALLY GOT THE ROTATION DONE
    F**K YOU 4 HOURS, Fly Gamemode zou nu moeten werken

--- Pizza Grower/Assets/Scripts/Jordan Scripts/FlyMovement.cs
+++ Pizza Grower/Assets/Scripts/Jordan Scripts/FlyMovement.cs
@@ -21,9 +21,7 @@ public class FlyMovement : MonoBehaviour
         {
             float step = Speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, Pizza.transform.position, step);
-            //transform.right = (4 * Pizza.transform.position - transform.position);
-            //Lookat();
-            //transform.LookAt((2 * transform.position - Pizza.transform.position));
+            Lookat();
         }
     }
 
@@ -35,17 +33,10 @@ public class FlyMovement : MonoBehaviour
         }
     }
 
-    /*void Lookat()
+    void Lookat()
     {
-        Vector3 targetDirection = Pizza.transform.position - transform.position;
-
-        // The step size is equal to speed times frame time.
-        float singleStep = speed * Time.deltaTime;
-
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.LookRotation(newDirection);
-    }*/
+        var dir = Pizza.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+    }
 }
